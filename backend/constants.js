5c49ee01c34faf090149b7b622c4556cdf2aa0dd
xray_os <xray_os@proton.me>
2025-07-25 18:57:39 -0400

    arreglando contenido

--- backend/constants.js
+++
@@ -1,5 +0,0 @@
-// c:\Respaldo Jhosber\Proyectos\Label\backend\constants.js
-export const SUPPORTED_CURRENCIES = [
-    'USD', 'VES', 'EUR', 'COP', 'BRL', 'MXN', 'CLP', 'PEN', 'ARS', 'UYU',
-    'DOP', 'GTQ', 'CRC', 'HNL', 'GBP', 'JPY', 'CAD', 'AUD', 'CHF', 'CNY'
-];
